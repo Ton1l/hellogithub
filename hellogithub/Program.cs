f9c897c04a057dd3fd826d94de18d3b9a12bf7d6
antonin.ondracek <antonin.ondracek@VT1-14>
2023-03-06 10:42:04 +0100

    Přidejte soubory projektu.

---
+++ hellogithub/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Ahoj, jsem Atnonín Ondráček, chodím na SŠ Educhem a mám rád:");
+
+string[] zajmy = { "skydiving", "snowboarding", "climbing" };
+foreach (var item in zajmy)
+{
+    Console.WriteLine(item);
+}
