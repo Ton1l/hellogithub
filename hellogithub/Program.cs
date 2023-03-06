7c908100b307d36c994c7e9dc0b274174fcb891e
antonin.ondracek <antonin.ondracek@VT1-14>
2023-03-06 10:50:50 +0100

    přidán text a zmenena barva textu

--- hellogithub/Program.cs
+++ hellogithub/Program.cs
@@ -1,6 +1,7 @@
-﻿Console.WriteLine("Ahoj, jsem Atnonín Ondráček, chodím na SŠ Educhem a mám rád:");
+﻿Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Ahoj, jsem Atnonín Ondráček, chodím na SŠ Educhem a mám rád:");
 
-string[] zajmy = { "skydiving", "snowboarding", "climbing" };
+string[] zajmy = { "skydiving", "snowboarding", "climbing", "smoking dat dark grown evil pack"};
 foreach (var item in zajmy)
 {
     Console.WriteLine(item);
